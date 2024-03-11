3086ec728b91e71f45703fc4d882bcc298c33d3b
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-03-11 23:57:40 +0530

    dp - fibbonacci

--- dp/fibb.cpp
+++ dp/fibb.cpp
@@ -1,61 +1,36 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
 using namespace std;
-int fibb(int n, vector<int> &dp)
-{
-    // step 1
-    if (n == 1 || n == 0)
-    {
-        return n;
-    }
-    // step3
-    if (dp[n] != -1)
-    {
-        return dp[n];
-    }
-    // step 2
-    dp[n] = fibb(n - 1, dp) + fibb(n - 2, dp);
+int fibbonacci(int n, vector<int>&dp){
+    if(n<=1) return n;
+    if(dp[n]!=-1)return dp[n];
+    dp[n] = fibbonacci(n-1,dp)+fibbonacci(n-2,dp);
     return dp[n];
 }
-
-int tbulation(int n)
-{
-    int dp[n + 1];
-    dp[1] = 1;
+int tabulation(int n){
+    vector<int> dp(n+1,-1);
     dp[0] = 0;
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i] = dp[i - 1] + dp[i - 2];
+    dp[1] = 1;
+    for(int i = 2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
     }
     return dp[n];
 }
-int spaceoptimize(int n)
-{
+int main(){
+    int  n;
+    cin >> n;
+    vector<int>dp(n+1,-1);
+    // cout << fibbonacci(n,dp);
+    // cout << tabulation(n);
 
-    int prev1 = 1;
     int prev2 = 0;
+    int prev1 = 1;
     int curr;
-    for (int i = 2; i <= n; i++)
-    {
-        curr = prev1 + prev2;
+    for(int i = 2;i<=n;i++){
+        curr = prev2+prev1;
         prev2 = prev1;
         prev1 = curr;
     }
-    return curr;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    vector<int> dp(n + 1, -1);
-    int val = fibb(n, dp);
-    cout << val << "  ";
-    cout << endl;
-    cout << " tablution : ";
-    int val2 = tbulation(n);
-    cout << val2 << " ";
-    cout << endl;
-    cout << " space optimize  : ";
-    int val3 = spaceoptimize(n);
-    cout << val3 << " ";
+    cout << curr << " ";
+
 }
